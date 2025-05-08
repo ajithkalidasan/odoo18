19c60cdeaeb358cd73a74c5c20812bd771898721
Ajith <joinwithajith@gmail.com>
2025-05-08 18:06:24 +0530

    Empty module created

---
+++ custom_addons/empty_module/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Empty Module',
+    'version': '1.0',
+    'category': 'Custom',
+    'summary': 'An empty module template',
+    'description': """
+        This is an empty module template that can be used as a starting point
+        for new Odoo 18 modules.
+    """,
+    'author': 'Your Name',
+    'website': 'https://www.yourwebsite.com',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+} 
---
+++ custom_addons/empty_module/models/__init__.py
@@ -0,0 +1,2 @@
+# This file is intentionally left empty
+# Add your model imports here when you create them 
